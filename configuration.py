8ab907d0014998c8775741684f94a069a06a1661
Obrivin <134394644+Obrivin@users.noreply.github.com>
2023-05-26 16:16:43 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://d119e42b-5fef-4ae2-a5b7-d777941938c0.serverhub.praktikum-services.ru"
+CREATE_A_NEW_ORDER = "/api/v1/orders"
+RECEIVING_AN_ORDER_BY_ID = "/api/v1/orders/track"
